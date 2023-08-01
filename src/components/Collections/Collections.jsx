e32fe6502aefec1daede1378de4fc0e73ef17513
Esteban Lopez <brandonlopezmonroy@gmail.com>
2023-08-01 08:11:37 -0500

    collection

---
+++ src/components/Collections/Collections.jsx
@@ -0,0 +1,148 @@
+import './Collection.css'
+import React from 'react';
+
+
+const Collection = () => {
+
+	return (
+		<section className='todo-collection'>
+            <section className='primer-div'>
+                <div>
+                    <h2>Collections</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                    <button>Shop</button>
+                </div>
+                <div>
+                    <div className='img'>img</div>
+                </div>
+            </section>
+            <section>
+                <h2>nuevos productos</h2>
+                <div className='flex'>
+                    <p>img</p>
+                    <p>img</p>
+                    <p>img</p>
+                    <p>img</p>
+                    <p>img</p>
+                </div>
+                <h2>Nuestros Productos</h2>
+                <div className='productos'>
+                    <div className='flex'>
+                        <div><p>img</p></div>
+                        <div>
+                            <p>hkdsj</p>
+                            <p>20000</p>
+                            <div>
+                                <button>sadfas</button>
+                                <button>sadfasd</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div><p>img</p></div>
+                        <div>
+                            <p>hkdsj</p>
+                            <p>20000</p>
+                            <div>
+                                <button>sadfas</button>
+                                <button>sadfasd</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div><p>img</p></div>
+                        <div>
+                            <p>hkdsj</p>
+                            <p>20000</p>
+                            <div>
+                                <button>sadfas</button>
+                                <button>sadfasd</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div><p>img</p></div>
+                        <div>
+                            <p>hkdsj</p>
+                            <p>20000</p>
+                            <div>
+                                <button>sadfas</button>
+                                <button>sadfasd</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='flex'>
+                <div>
+                    <p>lkjsadhfjashdkjasdfdasdfasdfhasd</p>
+                </div>
+                <div>
+                    <div className='img-redonda'>img</div>
+                    <button>askldj</button>
+                </div>
+            </section>
+            <section>
+                <p>rrewe</p>
+                <p>fkjsdhfjksdhflksd</p>
+                <div className='productoss'>
+                    <div>
+                        <div className='title'>
+                            <div>123</div>
+                            <dir>4.7</dir>
+                        </div>
+                        <p>sdkjfhasdjkfhskdl</p>
+                        <div className='title'>
+                            <div>img</div>
+                            <p>skhdkashd</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='title'>
+                            <div>123</div>
+                            <dir>4.7</dir>
+                        </div>
+                        <p>sdkjfhasdjkfhskdl</p>
+                        <div className='title'>
+                            <div>img</div>
+                            <p>skhdkashd</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='title'>
+                            <div>123</div>
+                            <dir>4.7</dir>
+                        </div>
+                        <p>sdkjfhasdjkfhskdl</p>
+                        <div className='title'>
+                            <div>img</div>
+                            <p>skhdkashd</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='productoss'>
+                <div>
+                    <div>img</div>
+                    <p>dkasdhask</p>
+                </div>
+                <div>
+                    <div>img</div>
+                    <p>dkasdhask</p>
+                </div>
+                <div>
+                    <div>img</div>
+                    <p>dkasdhask</p>
+                </div>
+                <div>
+                    <div>img</div>
+                    <p>dkasdhask</p>
+                </div>
+            </section>
+        </section>
+	)
+}
+
+
+
+export default Collection;
